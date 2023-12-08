e60fbecd6d717f77349709095213cb0f4f4bfa05
Mohamed Osama <36142935+osama51@users.noreply.github.com>
2023-12-08 11:42:24 +0200

    first commit: one tap sign in + firebase auth (bugged)

---
+++ app/src/main/java/com/toddler/recordit/LogIn.kt
@@ -0,0 +1,287 @@
+package com.toddler.recordit
+
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.auth.FirebaseAuth
+import com.toddler.recordit.ui.theme.Orange
+import com.toddler.recordit.ui.theme.RecordItTheme
+import com.toddler.recordit.ui.theme.Red
+
+//private lateinit var oneTapClient: SignInClient
+//private lateinit var signInRequest: BeginSignInRequest
+
+@Composable
+fun LoginScreen(isSignedIn: MutableState<Boolean>, firebaseAuth: MutableState<FirebaseAuth>) {
+    val activity = LoginActivity()
+    val id_token = stringResource(R.string.id_token)
+    // TODO: Login Screen
+    val context = LocalContext.current
+    val oneTapClient: SignInClient = remember { Identity.getSignInClient(context) }
+    val signInRequest: BeginSignInRequest = remember {
+        BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(id_token)
+                    .setFilterByAuthorizedAccounts(true)
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
+    }
+    val signUpRequest: BeginSignInRequest = remember {
+        BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(id_token)
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            )
+            .build()
+    }
+
+    RecordItTheme(darkTheme = false, dynamicColor = false) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Orange, Red
+                        )
+                    )
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "RecordIt",
+                    style = TextStyle(
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        fontSize = MaterialTheme.typography.displayMedium.fontSize,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontFamily = FontFamily.Cursive,
+                        letterSpacing = 10.sp,
+                        shadow = Shadow(
+                            color = MaterialTheme.colorScheme.onPrimary,
+                            blurRadius = 50f,
+                            offset = Offset(0f, 5f)
+                        )
+                    ),
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {
+                        activity.oneTapGoogleSignIn(
+                            oneTapClient,
+                            signInRequest,
+                            signUpRequest,
+                            firebaseAuth.value,
+                            isSignedIn
+                        )
+                    }
+                ) {
+                    Text(text = "Sign in")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {
+                    }
+                ) {
+                    Text(text = "Sign up")
+                }
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    RecordItTheme {
+        LoginActivityTest()
+    }
+}
+
+@Composable
+fun LoginActivityTest() {
+    RecordItTheme(darkTheme = false, dynamicColor = false) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Orange, Red
+                        )
+                    )
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val imageVector = ImageVector.vectorResource(id = R.drawable.ic_mic_54)
+                Image(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .fillMaxWidth(),
+
+                    imageVector = imageVector,
+                    contentDescription = "RecordIt Logo"
+                )
+                Text(
+                    text = "RecordIt",
+                    style = TextStyle(
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        fontSize = MaterialTheme.typography.displayMedium.fontSize,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontFamily = FontFamily.Cursive,
+                        letterSpacing = 10.sp,
+                        shadow = Shadow(
+                            color = MaterialTheme.colorScheme.onPrimary,
+                            blurRadius = 50f,
+                            offset = Offset(0f, 5f)
+                        )
+                    ),
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.onPrimary,
+                        contentColor = MaterialTheme.colorScheme.primary
+                    ),
+                    onClick = {},
+                    elevation = ButtonDefaults.buttonElevation(
+                        defaultElevation = 10.dp
+                    )
+                ) {
+                    Text(text = "Sign in")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {},
+                    elevation = ButtonDefaults.buttonElevation(
+                        defaultElevation = 10.dp
+                    )
+                ) {
+                    Text(text = "Sign up")
+                }
+            }
+
+
+        }
+    }
+}
+
+fun Modifier.vectorShadow(
+    path: Path,
+    x: Dp,
+    y: Dp,
+    radius: Dp
+) = composed(
+    inspectorInfo = {
+        name = "vectorShadow"
+        value = path
+        value = x
+        value = y
+        value = radius
+    },
+    factory = {
+
+        val paint = remember {
+            Paint()
+        }
+
+        val frameworkPaint = remember {
+            paint.asFrameworkPaint()
+        }
+
+        val color = Color.DarkGray
+        val dx: Float
+        val dy: Float
+        val radiusInPx: Float
+
+        with(LocalDensity.current) {
+            dx = x.toPx()
+            dy = y.toPx()
+            radiusInPx = radius.toPx()
+        }
+
+        drawBehind {
+            this.drawIntoCanvas {
+                val transparent = color
+                    .copy(alpha = 0f)
+                    .toArgb()
+
+                frameworkPaint.color = transparent
+
+                frameworkPaint.setShadowLayer(
+                    radiusInPx,
+                    dx,
+                    dy,
+                    color
+                        .copy(alpha = .7f)
+                        .toArgb()
+                )
+                it.drawPath(path, paint)
+            }
+        }
+    }
+)
---
+++ app/src/main/java/com/toddler/recordit/LoginActivity.kt
@@ -0,0 +1,51 @@
+package com.toddler.recordit
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.firebase.auth.FirebaseAuth
+import com.toddler.recordit.ui.theme.RecordItTheme
+
+
+class LoginActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RecordItTheme {
+                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+                    ToggleScreen()
+//                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun ToggleScreen() {
+    val firebaseAuth = remember { mutableStateOf(FirebaseAuth.getInstance()) }
+    val isSignedIn = remember { mutableStateOf(firebaseAuth.value.currentUser != null) }
+
+//    if (isSignedIn.value) {
+//        Toast.makeText(LocalContext.current, "Signed In", Toast.LENGTH_SHORT).show()
+//        HomeScreen(firebaseAuth.value)
+//    } else {
+        Toast.makeText(LocalContext.current, "Not Signed In", Toast.LENGTH_SHORT).show()
+        LoginScreen(isSignedIn, firebaseAuth) // pass firebaseAuth and isSignedIn as parameters
+//    }
+}
