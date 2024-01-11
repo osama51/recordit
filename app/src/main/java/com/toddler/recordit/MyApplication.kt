ad39f3d6ec5e74391b3590d909de357e5dcfd203
Mohamed Osama <36142935+osama51@users.noreply.github.com>
2024-01-11 04:19:48 +0200

    add firebase storage logic, download images.zip from storage to filesDir/images, import unzip utils from NitinParksash9911

--- app/src/main/java/com/toddler/recordit/MyApplication.kt
+++ app/src/main/java/com/toddler/recordit/MyApplication.kt
@@ -3,6 +3,7 @@ package com.toddler.recordit
 import android.app.Application
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import com.toddler.recordit.authentication.FirebaseAuthentication
 import dagger.hilt.android.HiltAndroidApp
 
@@ -10,6 +11,7 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class MyApplication: Application() {
     lateinit var firebaseAuth: FirebaseAuth
+    lateinit var storage: FirebaseStorage
 
     override fun onCreate() {
         super.onCreate()
@@ -21,5 +23,7 @@ class MyApplication: Application() {
         val firebaseApp = FirebaseApp.initializeApp(this)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
+
     }
 }
